79815a436cc3f1ded02107580f0e4dd635593478
juandiegomoscoso <programming.uxh5y@simplelogin.co>
2024-12-23 14:32:14 -0300

    Add argument parser for adding expenses

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Expenses tracker")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    
+    # Add expense
+    add_exp_parser = subparsers.add_parser("add_expense", help="Add a new expense")
+    add_exp_parser.add_argument("--description", required=True, help="Expense description")
+    add_exp_parser.add_argument("--amount", required=True, type=float, help="Expense amount")
+    add_exp_parser.add_argument("--category", help="Expense category")
+
