a56bf82f36a9c5599e5c6e082026cd52a1d0b439
juandiegomoscoso <programming.uxh5y@simplelogin.co>
2024-12-23 13:44:31 -0300

    Add method to parse the command line arguments
    - Add parse_arguments method

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import argparse
 from database.db_manager import DatabaseManager
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Expenses tracker")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    
